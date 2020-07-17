4f8316ec8f79e598388220b6b9e3c67c53b71cf3
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-07-16 22:13:38 -0400

    tests/test_main: Add tests for get_peer_pid_uid_gid()

--- tests/test_main.py
+++ tests/test_main.py
@@ -28,6 +28,35 @@ def test_get_peer_uid_gid_pair() -> None:
     assert unix_cred.get_peer_uid_gid(sock_b.fileno()) == (os.geteuid(), os.getegid())
 
 
+if sys.platform.startswith(("linux", "openbsd", "netbsd", "freebsd", "solaris", "illumos")):
+
+    def test_get_peer_pid_uid_gid(tmp_path: pathlib.Path) -> None:
+        cli, server_cli = util.sock_bind_connect_accept(tmp_path / "sock")
+
+        for (pid, uid, gid) in [
+            unix_cred.get_peer_pid_uid_gid(cli),
+            unix_cred.get_peer_pid_uid_gid(cli.fileno()),
+            unix_cred.get_peer_pid_uid_gid(server_cli),
+            unix_cred.get_peer_pid_uid_gid(server_cli.fileno()),
+        ]:
+            assert uid == os.geteuid()
+            assert gid == os.getegid()
+            assert pid in (0, os.getpid())
+
+    def test_get_peer_pid_uid_gid_pair() -> None:
+        sock_a, sock_b = socket.socketpair(socket.AF_UNIX)
+
+        for (pid, uid, gid) in [
+            unix_cred.get_peer_pid_uid_gid(sock_a),
+            unix_cred.get_peer_pid_uid_gid(sock_a.fileno()),
+            unix_cred.get_peer_pid_uid_gid(sock_b),
+            unix_cred.get_peer_pid_uid_gid(sock_b.fileno()),
+        ]:
+            assert uid == os.geteuid()
+            assert gid == os.getegid()
+            assert pid in (0, os.getpid())
+
+
 if sys.platform.startswith(("openbsd", "netbsd", "freebsd", "dragonfly", "darwin")):
 
     def test_getpeereid(tmp_path: pathlib.Path) -> None:
