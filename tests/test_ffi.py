a741b931416864c53dabb75b5d71c0791b77e1ff
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-09-08 22:44:19 -0400

    tests: Add test for unix_cred.ffi.load_libc()

---
+++ tests/test_ffi.py
@@ -0,0 +1,16 @@
+import ctypes
+import os
+
+import unix_cred
+
+
+def test_load_libc() -> None:
+    libc = unix_cred.ffi.load_libc()
+    assert libc is not None
+
+    # Returns the same library
+    assert libc is unix_cred.ffi.load_libc()
+
+    libc.nice.argtypes = (ctypes.c_int,)
+    libc.nice.restype = ctypes.c_int
+    assert libc.nice(0) == os.nice(0)
